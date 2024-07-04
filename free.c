9dbb15a722ab5b25d189dc77ada3f77f12c996ef
GouzGouz <me@gmail.fr>
2024-07-04 20:37:22 +0200

    do realloc and we good (maybe will threadsafe should be easy)

--- free.c
+++ free.c
@@ -1,8 +1,5 @@
 #include "ft_malloc.h"
 
-// le pointeur - 1 == size du chunk a free
-// le size du chunk == -1 si il est free
-
 static char* find_start(char* to_find)
 {
     char *ptr;
--- realloc.c
+++ realloc.c
@@ -4,7 +4,12 @@ void *realloc(void *ptr, size_t size)
 {
     if (GET_CHUNK_SIZE(memory_pool + sizeof(size_t)) == 0)
         init_malloc();
-    (void) ptr;
-    (void) size;
-    return NULL;
+    if (!ptr)
+        return my_malloc(size);
+    if (ptr && size == 0)
+    {
+        my_free(ptr);
+        return NULL;
+    }
+    //start realloc functions logic
 }
